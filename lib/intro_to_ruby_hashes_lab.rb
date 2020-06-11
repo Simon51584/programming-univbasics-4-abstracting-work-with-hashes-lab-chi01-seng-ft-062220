abe7f8844dfac72056100122428d5727efc68e97
Seth Starr <starrseth@hotmail.com>
2020-06-11 01:01:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,9 @@ end
 
 def update_counting_hash(hash, key)
   hash[key]
+  if hah[key] = nil 
+    hash => key
+  end 
   # given a hash an a key as parameters, return an updated hash
   
   # if the provided key is not present in the hash, add it and assign it to the value of 1
